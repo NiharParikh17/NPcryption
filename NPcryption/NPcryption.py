7df48ddab41fd8a596b2981c052a2ed33bec6e26
Nihar Parikh <nihar17999@gmail.com>
2021-05-02 13:18:59 -0400

    Removing existing running code replacing GUI

--- NPcryption/NPcryption.py
+++ NPcryption/NPcryption.py
@@ -10,18 +10,10 @@ def decrypt(P, Q, E, message):
     """Decrypting the message by calling decrypt function in Decryption"""
     return Decryption.decrypt(P, Q, E, message)
 
+def buildGUI():
+    window = tk.Tk()
+
 def main():
-    answer = input("Press E for Encryption or D for Decryption: ")
-    P = int(input("Enter Private Key P: "))
-    Q = int(input("Enter Private Key Q: "))
-    E = int(input("Enter Public Key E: "))
-
-    if answer == "E" or answer == "e":
-        message = input("Enter Message: ")
-        print(encrypt(P, Q, E, message))
-
-    elif answer == "D" or answer == "d":
-        message = input("Encrypted Message: ")
-        print(decrypt(P, Q, E, message))
+    print("GUI in progress!")
 
 main()
